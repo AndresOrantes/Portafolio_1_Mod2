7c8dcfb9c8e15a8cfacaf3769788e890aa759b23
Andres Orantes <jagorantes10@gmail.com>
2024-08-26 19:20:15 -0600

    GPAPredic

--- GPAPredicyourGrade.py
+++ GPAPredicyourGrade.py
@@ -1,4 +1,4 @@
-#GPA
+#GPA, pero esta vez predice tu calificación en base a tu
 #Implementación de una técnica de aprendizaje máquina sin el uso de un framework
 #========================================================================================================#
 #========================================================================================================#
@@ -71,25 +71,18 @@ def categorizacion_gpa(gpa):
     else:
         return 2#Baja
 df['GPA_Categoria'] = df['GPA'].apply(categorizacion_gpa)#Hace una nueva columna categórica que es numérica.
-#Vamos a usar los features numéricos y se convertiran en numéricos los que no lo son.
-df = pd.get_dummies(df, columns=['Major'], drop_first=True)
 
 #Hacemos columnas binarias
-df['Gender'] = df['Gender'].replace({'Female': 1, 'Male': 0})#
 df['PartTimeJob'] = df['PartTimeJob'].replace({'Yes': 1, 'No': 0})#
 df['ExtraCurricularActivities'] = df['ExtraCurricularActivities'].replace({'Yes': 1, 'No': 0})#
-df['Major_Business'] = df['Major_Business'].astype(int)
-df['Major_Education'] = df['Major_Education'].astype(int)
-df['Major_Engineering'] = df['Major_Engineering'].astype(int)
-df['Major_Science'] = df['Major_Science'].astype(int)
+df['Gender'] = df['Gender'].replace({'Female': 1, 'Male': 0})#
 
-
-y_alto = (df["GPA_Categoria"] == 0).astype(int) # return 1 if Iris Setosa (0 = setosa, 1 = versicolor, 2 = virginica), and 0 if not
+y_alto = (df["GPA_Categoria"] == 0).astype(int) # 
 y_medio = (df["GPA_Categoria"] == 1).astype(int)
 y_bajo = (df["GPA_Categoria"] == 2).astype(int)
 
 
-X=df.drop(columns=['GPA_Categoria','GPA','StudentID'])
+X=df.drop(columns=['GPA_Categoria','GPA','StudentID','Gender','Major','ExtraCurricularActivities','PartTimeJob'])
 
 # Listq de ys
 y_niveles = [y_alto, y_medio, y_bajo]
@@ -103,35 +96,57 @@ actual_y = {'GPA Alto':0,
             'GPA Medio':0,
             'GPA Bajo':0}
 
+valido=False
+"""while valido==0 :
+  predecir = input("Ingrese los valores para las características separados por comas: ")
+  predecir = [float(x) for x in predecir.split(',')]
+  if len(predecir) != X.shape[1]:
+    print(f"Por favor, ingrese {X.shape[1]} características.")
+  else:
+    valido=True"""
 
+
+#Es más sencillo para todos si lo hago así
+while valido==False:
+  Edad =int(input("Ingresa tu edad: "))
+  Horas=int(input("Ingresa las horas que invertiste en tu estudio: "))
+  Asistencias=int(input("Ingresa tu asistencia(0-100): "))
+  if Edad!=None and Horas!=None and Asistencias!=None:
+    predecir=(Edad, Horas, Asistencias)
+    valido=True
+  else:
+    print('Revisa y vuelve a ingresar tus datos')
+
+
+escalarysesgo=False
 for key, y_nivel in y_niveles.items():
   # Split dataset (training and testing sets)
-  X_train, X_test, y_train, y_test = train_test_split(X, y_nivel, test_size=0.2)
-
+  #X_train, X_test, y_train, y_test = train_test_split(X, y_nivel, test_size=0.2)
+  X_train=X #Pues no creo que sea necesario la separación.
+  y_train=y_nivel
   # Scale X
   sc = StandardScaler()
   X_train = sc.fit_transform(X_train)
-  X_test = sc.transform(X_test)
   # Train model
   epochs = 1000
   alpha = 1
   best_params = log_regression4(X_train, y_train, alpha, epochs)
-  # Make predictions on test set
-  index_ = 0
-  X_to_predict = [list(X_test[index_])]
-  # print(X_to_predict)
-  X_to_predict = np.c_[np.ones((len(X_to_predict), 1)), X_to_predict] # add x0 for bias
-  # print(X_to_predict)
-  pred_probability = sigmoid_function(X_to_predict.dot(best_params))
+
+  if escalarysesgo==False:
+    # Paréntesis: se escalan los datos ingresados, pero se tienen que escalar con el standard scaler,
+    #y el StandardScaler está dentro del for, por lo que se me ocurrió hacer esto, para que se prediga una vez
+    predecir = sc.fit_transform([predecir]) 
+    predecir = np.c_[np.ones((len(predecir), 1)), predecir] #Se añade x0 para el sesgo
+    escalarysesgo=True
+
+  # Predicción y su probabilidad
+  pred_probability = sigmoid_function(predecir.dot(best_params)) 
   predicted_probs[key] = pred_probability[0][0]
   print('Our model calculated probability of sample being {}, is: {}%'.format(key, round(pred_probability[0][0]*100,2)))
-  actual_y[key] = y_test.iloc[index_]
-
+  #print('El modelo calculó que la probabilidad de que la muestra sea', key, 'es:',round(pred_probability[0][0]*100,2))
 max_key = max(predicted_probs, key=predicted_probs.get)
-print('\n', predicted_probs)
-print('\nModel Prediction: {}'.format(max_key))
-max_actual_y = max(actual_y, key=actual_y.get)
-print('Real value is: {}'.format(max_actual_y))
-
-print(X_train)
-print(X_test)
+print("=========================================================================")
+print("=========================================================================")
+print("=========================================================================")
+print("=========================================================================")
+print('\La predicción indica que es probable que tengas un {}'.format(max_key))
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Momento de Retroalimentación: Módulo 2 Implementación de una técnica de apre
 En este repositorio se encuentra el set de datos y el código realizados.
 A mi parecer no tiene el mejor rendimiento el modelo de predicción de GPA debido a falta de datos para GPA Alto
 El de género es mejor para predecir, se puede deber a la gran cantidad de features con los que contamos. Sin embargo, el modelo se mantiene con una alta probabilidad de que sea mujer aunque sea hombre. Aunque de cualquier forma realiza la predicción correcta.
+GPAPredicyourGrade: Este archivo genera una predicción de como será tu GPA en base a los datos que tú des.
